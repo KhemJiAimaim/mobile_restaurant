c9437b6d2ab78be68466743fecd88ba676d47147
Nattapol <nattapol.surinkeaw@gmail.com>
2025-03-05 13:32:41 +0700

    b_update_api_path

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import axios from 'axios'
+import { api_path } from './pages/store/setting'
+
+axios.interceptors.request.use((request) => {
+  request.headers.Authorization = `Bearer ${localStorage.getItem("accessToken")}`;
+  request.url = `${api_path}${request.url}`;
+  return request;
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
